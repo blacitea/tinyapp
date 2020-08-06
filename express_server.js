028b9ebc9b00472c6cb49f7c70d24525f096ccaa
blacitea <angelhoikei@gmail.com>
2020-08-06 19:23:09 +0000

    fixed the POST /logout to redirect to '/' instead of '/urls'

--- express_server.js
+++ express_server.js
@@ -144,7 +144,7 @@ app.post("/login", (req, res) => {
 
 app.post("/logout", (req, res) => {
   res.clearCookie('user_id');
-  res.redirect("/urls");
+  res.redirect("/");
 });
 
 // New user registration
