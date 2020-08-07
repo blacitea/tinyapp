9f175e3169b32c849aac551002b968de1d4d0abc
blacitea <angelhoikei@gmail.com>
2020-08-07 00:46:29 +0000

    tidy up

--- const.js
+++ const.js
@@ -7,11 +7,15 @@ const unloggedUser = {
 // Error template
 const unauthUser = `Unauthorized request, please re-login. --> <a href="/login">LOGIN HERE</a>`;
 const loginRequired = `Login required. --> <a href="/login">LOGIN HERE</a>`;
+const directLogin = `\n --> <a href="/login">LOGIN HERE</a>`;
+const directRegister = `\n --> <a href="/register">REGISTER HERE</a>`;
 
 
 module.exports = {
   PORT,
   unloggedUser,
   unauthUser,
-  loginRequired
+  loginRequired,
+  directLogin,
+  directRegister
 };
--- express_server.js
+++ express_server.js
@@ -12,26 +12,28 @@ const { PORT, unloggedUser, unauthUser, loginRequired, directLogin, directRegist
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieSession({
-  name: 'Waffle',
-  keys: ['neroIStheBEST']
-}));
+app.use(cookieSession({ name: 'Waffle', keys: ['neroIStheBEST']}));
 
 
 app.get("/", (req, res) => {
+
   if (loggedUser(req)) {
     res.redirect('/urls');
+    
   } else {
     res.redirect('/login');
   }
 });
 
 app.get("/urls", (req, res) => {
+
   if (!loggedUser(req)) {
     res.status(401).send(loginRequired);
+
   } else if (!userDB[loggedUser(req)]) {
     req.session = null;
     res.redirect('/login');
+
   } else {
     let userURLs = urlsForUser(urlDatabase, loggedUser(req));
     let templateVars = {
@@ -43,11 +45,13 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
+
   if (loggedUser(req)) {
     let templateVars = {
       email: userDB[loggedUser(req)].email,
     };
     res.render('urls_new', templateVars);
+
   } else {
     res.redirect('/login');
   }
@@ -56,14 +60,18 @@ app.get("/urls/new", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
   let shortURL = req.params.shortURL;
   let urlObj = findObjByURL(urlDatabase, req.params.shortURL);
+
   if (!urlObj) {
     res.status(404).redirect(`https://http.cat/404`);
+
   } else if (!userDB[loggedUser(req)]) {
     res.status(401).send(loginRequired);
+
   } else if (urlObj.userID !== loggedUser(req)) {
     res.statusCode = 401;
     req.session = null;
     res.send(unauthUser);
+
   } else {
     let templateVars = {
       shortURL,
@@ -77,8 +85,10 @@ app.get("/urls/:shortURL", (req, res) => {
 
 
 app.get('/register', (req, res) => {
+
   if (loggedUser(req)) {
     res.redirect('/urls');
+
   } else {
     res.render('urls_register', unloggedUser);
   }
@@ -86,16 +96,20 @@ app.get('/register', (req, res) => {
 
 app.get('/u/:shortURL', (req, res) => {
   let urlObj = findObjByURL(urlDatabase, req.params.shortURL);
+
   if (urlObj) {
     res.redirect(302, urlObj.longURL);
+
   } else {
     res.status(404).redirect(`https://http.cat/404`);
   }
 });
 
 app.get("/login", (req, res) => {
+
   if (loggedUser(req)) {
     res.redirect('/urls');
+
   } else {
     res.render('urls_login', unloggedUser);
   }
@@ -103,11 +117,14 @@ app.get("/login", (req, res) => {
 
 app.post("/login", (req, res) => {
   let userID = findUserIDByEmail(userDB, req.body.email);
+
   if (userID && bcrypt.compareSync(req.body.password, userDB[userID].password)) {
     req.session.userId = userID;
     res.redirect(`/urls`);
+
   } else if (userID) {
     res.status(400).send(`Incorrect password. ${directLogin}`);
+
   } else {
     res.status(404).send(`Email not registered ${directRegister}`);
   }
@@ -121,10 +138,13 @@ app.post("/logout", (req, res) => {
 // New user registration
 app.post("/register", (req, res) => {
   let tempID = generateRandomString();
+
   if (!req.body.email || !req.body.password) {
     res.status(400).send(`Registration failed. \nEmail and/or Password cannot be empty ${directRegister}`);
+
   } else if (findUserIDByEmail(userDB, req.body.email)) {
     res.status(400).send(`Registration failed. \nEmail address already registered ${directLogin}`);
+    
   } else {
     const hash = bcrypt.hashSync(req.body.password, 10);
     let registerData = {
@@ -140,6 +160,7 @@ app.post("/register", (req, res) => {
 
 // Create new shortURL
 app.post("/urls", (req, res) => {
+
   if (loggedUser(req)) {
     let tempURL = generateRandomString();
     urlDatabase[tempURL] = {
@@ -148,6 +169,7 @@ app.post("/urls", (req, res) => {
       create: new Date().toLocaleDateString(),
     };
     res.redirect(`/urls/${tempURL}`);
+
   } else {
     res.statusCode = 401;
     req.session = null;
@@ -158,9 +180,11 @@ app.post("/urls", (req, res) => {
 // Delete URLs
 app.post("/urls/:shortURL/delete", (req, res) => {
   let shortURL = req.params.shortURL;
+
   if (loggedUser(req) && loggedUser(req) === urlDatabase[shortURL].userID) {
     delete (urlDatabase[shortURL]);
     res.redirect(`/urls`);
+
   } else {
     res.statusCode = 401;
     req.session = null;
@@ -171,9 +195,11 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 // Edit request from individual URL page
 app.post("/urls/:shortURL", (req, res) => {
   let urlObj = findObjByURL(urlDatabase, req.params.shortURL);
+
   if (loggedUser(req) && loggedUser(req) === urlObj.userID) {
     urlObj.longURL = req.body.longURL;
     res.redirect(`/urls`);
+
   } else {
     res.statusCode = 401;
     req.session = null;
